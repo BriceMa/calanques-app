861147157d43ca286f0135f41255ed8d1c09125d
Brice <>
2019-04-02 10:50:59 +0200

    Initialize project

---
+++ Les Calanques/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Les Calanques
+//
+//  Created by Brice on 02/04/2019.
+//  Copyright © 2019 Brice Mangeat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Les Calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Les Calanques
+//
+//  Created by Brice on 02/04/2019.
+//  Copyright © 2019 Brice Mangeat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
